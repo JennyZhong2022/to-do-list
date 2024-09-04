6c179d6b223be12926e16d6535bdf55c7df80660
Xiao Zhong <zhongxiao2017@gmail.com>
2024-09-04 23:18:27 +1000

    frontend responsive ui update

--- front-end/src/components/ToDoPost/ToDoPost.tsx
+++ front-end/src/components/ToDoPost/ToDoPost.tsx
@@ -1,10 +1,17 @@
 import { ToDoPostResponse } from "../../services/todo-post";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-dayjs.extend(relativeTime);
-import styles from "./ToDoPost.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClone, faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
+import {
+  faClone,
+  faTrash,
+  faPen,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useRef, useState } from "react";
+import styles from "./ToDoPost.module.scss";
+
+dayjs.extend(relativeTime);
 
 interface ToDoPostProps {
   post: ToDoPostResponse;
@@ -13,6 +20,26 @@ interface ToDoPostProps {
 }
 
 const ToDoPost = ({ post, onDelete, onEdit }: ToDoPostProps) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
       <input type="checkbox" className={styles.checkbox} />
@@ -20,25 +47,51 @@ const ToDoPost = ({ post, onDelete, onEdit }: ToDoPostProps) => {
       <div className={styles.taskContainer}>
         <p className={styles.content}>{post.content}</p>
         <h3 className={styles.category}>{post.category.name}</h3>
-        <div className={styles.buttonsContainer}>
-          <button className={styles.duplicateBtn}>
-            <FontAwesomeIcon icon={faClone} className="fa-2x" />
+
+        <div className={styles.actionsWrapper} ref={menuRef}>
+          {/* show action icons for larger screens */}
+          <div className={styles.actionIcons}>
+            <button className={styles.duplicateBtn}>
+              <FontAwesomeIcon icon={faClone} className="fa-2x" />
+            </button>
+            <button onClick={onEdit} className={styles.editBtn}>
+              <FontAwesomeIcon icon={faPen} className="fa-2x" />
+            </button>
+            <button
+              onClick={() => onDelete(post.id)}
+              className={styles.deleteBtn}
+            >
+              <FontAwesomeIcon icon={faTrash} className="fa-2x" />
+            </button>
+          </div>
+
+          {/* show menu button for smaller screens */}
+          <button className={`${styles.dropdownBtn}`} onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faBars} className="fa-1x" />
           </button>
 
-          <button onClick={onEdit} className={styles.editBtn}>
-            <FontAwesomeIcon icon={faPen} className="fa-2x" />
-          </button>
-          <button
-            onClick={() => onDelete(post.id)}
-            className={styles.deleteBtn}
-          >
-            <FontAwesomeIcon icon={faTrash} className="fa-2x" />
-          </button>
+          {/* show dropdown menu for smaller screens */}
+          {menuOpen && (
+            <div className={styles.dropdownMenu}>
+              <button className={styles.duplicateBtn}>
+                <FontAwesomeIcon icon={faClone} className="fa-1x" />
+                Duplicate
+              </button>
+              <button onClick={onEdit} className={styles.editBtn}>
+                <FontAwesomeIcon icon={faPen} className="fa-1x" />
+                Edit
+              </button>
+              <button
+                onClick={() => onDelete(post.id)}
+                className={styles.deleteBtn}
+              >
+                <FontAwesomeIcon icon={faTrash} className="fa-1x" />
+                Delete
+              </button>
+            </div>
+          )}
         </div>
       </div>
-
-      {/* <h4 className={styles.createTime}>CreateAt:{dayjs(post.createdAt).fromNow()}</h4> */}
-      {/* <h4>UpdateAT:{dayjs(post.updatedAt).fromNow()}</h4> */}
     </div>
   );
 };
