11536822930948bdd02fd2f3e64c45a6ff5ffdaa
Xiao Zhong <zhongxiao2017@gmail.com>
2024-09-30 22:01:35 +1000

    update url for deployment

--- todolist/src/main/java/nology/todolist/config/WebConfig.java
+++ todolist/src/main/java/nology/todolist/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     String[] allowedOrigins = { "http://localhost:5173/",
-        "http://127.0.0.1:5173/" };
+        "http://127.0.0.1:5173/", "http://jennytodo-list.s3-website-ap-southeast-2.amazonaws.com" };
     registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("*").allowedHeaders("*");
   }
 }
